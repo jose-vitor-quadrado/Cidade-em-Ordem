1c01745276b6e9f67d905e76593614c2eb9ec6cd
Jose Vitor <josevitorquadrado@gmail.com>
2026-09-17 15:21:25 -0300

    feat: adiciona as classes endereco e ordem e os enumeradores tipoProblema e prioridade

---
+++ CidadeEmOrdem/Models/Endereco.cs
@@ -0,0 +1,17 @@
+﻿namespace CidadeEmOrdem.Models;
+
+public class Endereco
+{
+    public int Cep { get; set; }
+    public string Cidade { get; set; } = string.Empty;
+    public string Bairro { get; set; } = string.Empty;
+    public string Logradouro { get; set; } = string.Empty;
+
+    public Endereco(int cep, string cidade, string bairro, string logradouro)
+    {
+        Cep = cep;
+        Cidade = cidade;
+        Bairro = bairro;
+        Logradouro = logradouro;
+    }
+}
---
+++ CidadeEmOrdem/Models/Ordem.cs
@@ -0,0 +1,22 @@
+﻿namespace CidadeEmOrdem.Models;
+
+public class Ordem
+{
+    public int Id { get; set; }
+    public string Descricao { get; set; } = string.Empty;
+    public TipoProblema TipoProblema { get; set; }
+    public Prioridade Prioridade { get; set; }
+    public Endereco? Endereco { get; set; }
+    public string? ImagemUrl { get; set; }
+
+    public Ordem(int id, string descricao, TipoProblema tipoProblema, 
+        Prioridade prioridade, Endereco? endereco, string? imagemUrl)
+    {
+        Id = id;
+        Descricao = descricao;
+        TipoProblema = tipoProblema;
+        Prioridade = prioridade;
+        Endereco = endereco;
+        ImagemUrl = imagemUrl;
+    }
+}
