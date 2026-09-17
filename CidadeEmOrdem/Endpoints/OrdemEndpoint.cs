8b6e84238ecde9e43fc977ae8fd51574ccb3e2f1
Jose Vitor <josevitorquadrado@gmail.com>
2026-09-17 11:19:40 -0300

    Add: Cria a estrutura base do projeto

---
+++ CidadeEmOrdem/Endpoints/OrdemEndpoint.cs
@@ -0,0 +1,15 @@
+﻿namespace CidadeEmOrdem.Endpoints;
+
+/* 
+ * Nesta classe ficarão todas as rotas relacionadas
+ * ao envio dos problemas
+*/
+public static class OrdemEndpoint
+{
+    public static WebApplication MapOrdemEndpoints(this WebApplication app)
+    {
+
+
+        return app;
+    }
+}
---
+++ CidadeEmOrdem/Extensions/EndpointExtensions.cs
@@ -0,0 +1,16 @@
+﻿using CidadeEmOrdem.Endpoints;
+
+namespace CidadeEmOrdem.Extensions;
+
+/*
+ * Nesta classe nos chamamos as rotas sem precisar deixar
+ * o Program.cs cheio de extensões
+*/
+public static class EndpointExtensions
+{
+    public static void MapEndpoints(this WebApplication app)
+    {
+        // Mapeando elas abaixo
+        app.MapOrdemEndpoints();
+    }
+}
