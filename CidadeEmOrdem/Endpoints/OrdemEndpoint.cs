4b0471e2e50b3ed49dbbb755d352c59f19f7889b
Jose Vitor <josevitorquadrado@gmail.com>
2026-09-18 15:53:52 -0300

    feat: adiciona os repositories e os services de ordem

--- CidadeEmOrdem/Endpoints/OrdemEndpoint.cs
+++ CidadeEmOrdem/Endpoints/OrdemEndpoint.cs
@@ -1,4 +1,6 @@
-﻿namespace CidadeEmOrdem.Endpoints;
+﻿using CidadeEmOrdem.Models;
+
+namespace CidadeEmOrdem.Endpoints;
 
 /* 
  * Nesta classe ficarão todas as rotas relacionadas
@@ -9,6 +11,46 @@ public static class OrdemEndpoint
 {
     public static WebApplication MapOrdemEndpoints(this WebApplication app)
     {
+        app.MapGet("/problemas", () =>
+        {
+            var lista = new List<Ordem>
+            {
+                new Ordem
+                {
+                    Descricao = "Buraco na rua principal",
+                    TipoProblema = TipoProblema.Buraco,
+                    Prioridade = Prioridade.Alta,
+                    Endereco = new Endereco
+                    {
+                        Estado = "SP",
+                        Cidade = "Sjrp",
+                        Cep = "15560-098",
+                        Bairro = "Sao Joao",
+                        Logradouro = "Rua Sao Vicente de Paula 654"
+                    },
+                    ImagemUrl = "https://exemplo.com/buraco.jpg",
+                    FoiResolvido = false
+                },
+                new Ordem
+                {
+                    Descricao = "Arvore maluca atacando os pedestres",
+                    TipoProblema = TipoProblema.Arvore,
+                    Prioridade = Prioridade.Media,
+                    Endereco = new Endereco
+                    {
+                        Estado = "SP",
+                        Cidade = "Palestina",
+                        Cep = "15560-078",
+                        Bairro = "Tem bairro",
+                        Logradouro = "Aquela la daquele lugar 123"
+                    },
+                    ImagemUrl = "https://exemplo.com/arvore-do-mal.jpg",
+                    FoiResolvido = false
+                }
+            };
+
+            return lista;
+        });
 
         return app;
     }
---
+++ CidadeEmOrdem/Services/IOrdemService.cs
@@ -0,0 +1,18 @@
+﻿using CidadeEmOrdem.Models;
+
+namespace CidadeEmOrdem.Services;
+
+public interface IOrdemService
+{
+    Ordem? GetById(int id);
+    List<Ordem> GetAll();
+    List<Ordem> GetByTipoProblema(TipoProblema tipoProblema);
+    List<Ordem> GetByPrioridade(Prioridade prioridade);
+    List<Ordem> GetByEstado(string estado);
+    List<Ordem> GetByCidade(string cidade);
+    List<Ordem> GetByBairro(string bairro);
+    List<Ordem> GetByLogradouro(string logradouro);
+    Ordem? CriarOrdem(Ordem ordem);
+    Ordem? AtualizarOrdem(int id, Ordem ordem);
+    bool RemoverOrdem(int id);
+}
